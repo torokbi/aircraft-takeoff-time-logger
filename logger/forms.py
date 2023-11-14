e3e835d8fa7b8f2034e9bd3628505484d36e59cd
torokbi <bence.torok50@gmail.com>
2023-11-14 21:32:01 +0100

    Make the initial flask version of the app

--- logger/forms.py
+++ logger/forms.py
@@ -1,2 +1,7 @@
-from dataclasses import field
-from django import forms
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class NewPlane(FlaskForm):
+    reg = StringField('Lajstrom', validators=[DataRequired(), Length(min=2)])
+    submit = SubmitField('Felszállít')
